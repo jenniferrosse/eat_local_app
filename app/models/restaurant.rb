b2b966dfbade41e6dcfa5d4690418d3979b9f2f4
Jennifer Rosse <jenniferrosse26@gmail.com>
2016-08-26 21:57:34 -0500

    Styled forms

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,7 @@
 class Restaurant < ActiveRecord::Base
 	# attr_accessible :address, :latitude, :longitude
+  validates :name, :address, :url, presence: true
+
 	geocoded_by :address
 	after_validation :geocode, :if => :address_changed?
 end
