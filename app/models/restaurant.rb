06c710ed3fd7dbbe4dc543e7870fbe6bcddddb98
Jennifer Rosse <jenniferrosse26@gmail.com>
2016-09-04 11:13:09 -0500

    Removed admin, edit links

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,7 @@
 class Restaurant < ActiveRecord::Base
 	# attr_accessible :address, :latitude, :longitude
   validates :name, :address, :url, presence: true
+  validates :name, :address, :url, uniqueness: true
 
 	geocoded_by :address
 	after_validation :geocode, :if => :address_changed?
