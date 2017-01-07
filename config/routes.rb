cfe8f9692ee5a57b94516a99577c68d2084a0c8d
Jennifer Rosse <jenniferrosse26@gmail.com>
2017-01-06 18:46:21 -0600

    added view for atlanta

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
 controller :restaurants do
   get :restaurant_list
+  get :restaurants_atlanta
 end
 
   # The priority is based upon order of creation: first created -> highest priority.
